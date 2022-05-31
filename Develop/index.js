45467f09feb3ea7b2772002208bed0e8266d72b8
vhivestate <garcia.valeria001@gmail.com>
2022-05-31 01:37:41 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+        }
+    ]);
+};
+questions()
+.then(answers => {
+    console.log(answers)});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
